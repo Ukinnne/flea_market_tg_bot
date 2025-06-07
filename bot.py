d45f43aec9dd89ebf6b9ded53d39d8cdd6737274
Ukinnne <osolovegor@gmail.com>
2025-06-08 04:21:22 +1000

    Correcting errors due to the difference in aiogram versions and adding processing of the start command

--- bot.py
+++ bot.py
@@ -1,13 +1,23 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.filters import Command
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
 dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer("<b>Привет!</b>")
 
 async def main():
     await dp.start_polling(bot)
